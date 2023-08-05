7903bff0ba6d8b1d6dd8b18257b7cb41967e6549
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-08-05 08:35:13 +0530

    added details to readme

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
 const { PORT } = require('./config/serverConfig');
+const bodyParser = require('body-parser');
 
 const server = async () => {
 
 	const app = express();
+
+	//middleware
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true}));
 
 	app.listen(PORT, ()=> {
 		console.log("server is started on :", PORT);
