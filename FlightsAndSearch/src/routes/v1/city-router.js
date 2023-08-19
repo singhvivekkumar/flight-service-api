c6e4b57f5cd08f8cd23a84869187415deee535c2
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-08-19 09:10:30 +0530

    make new conection

--- FlightsAndSearch/src/routes/v1/city-router.js
+++ FlightsAndSearch/src/routes/v1/city-router.js
@@ -3,10 +3,16 @@ const cityControllers = require('../../controllers/city-controllers')
 
 const CityRouter = express.Router();
 
-CityRouter.post('/city', cityControllers.create);
-CityRouter.patch('/city/:id', cityControllers.update);
-CityRouter.get('/city/:id', cityControllers.get);
-CityRouter.get('/city', cityControllers.getAll);
-CityRouter.delete('/city/:id', cityControllers.destroy);
+// CityRouter.post('/city', cityControllers.create);
+// CityRouter.patch('/city/:id', cityControllers.update);
+// CityRouter.get('/city/:id', cityControllers.get);
+// CityRouter.get('/city', cityControllers.getAll);
+// CityRouter.delete('/city/:id', cityControllers.destroy);
+
+CityRouter.post('/', cityControllers.create);
+CityRouter.patch('/:id', cityControllers.update);
+CityRouter.get('/:id', cityControllers.get);
+CityRouter.get('/', cityControllers.getAll);
+CityRouter.delete('/:id', cityControllers.destroy);
 
 module.exports = CityRouter;
--- FlightsAndSearch/src/routes/v1/index.js
+++ FlightsAndSearch/src/routes/v1/index.js
@@ -1,37 +1,37 @@
-// const express = require('express');
+const express = require('express');
 
-// const v1Router = express.Router();
+const v1Router = express.Router();
 
-// const city = require('./city-router');
-// const flight = require('./flight-router')
+const city = require('./city-router');
+const flight = require('./flight-router')
 
-// v1Router.use('/city', city);
-// v1Router.use('/flight', flight);
+v1Router.use('/city', city);
+v1Router.use('/flight', flight);
 
-// module.exports = v1Router;
+module.exports = v1Router;
 
 // module.exports = {
 // 	'/city': require('./city-router'),
 // 	'/flight': require('./flight-router')
 // }
 
-const express = require('express');
-const cityController = require('../../controllers/city-controllers');
-const flightController = require('../../controllers/flight-controller');
-const { FlightMiddleware } = require('../../middlewares/index')
-const router = express.Router();
+// const express = require('express');
+// const cityController = require('../../controllers/city-controllers');
+// const flightController = require('../../controllers/flight-controller');
+// const { FlightMiddleware } = require('../../middlewares/index')
+// const router = express.Router();
 
-// these are for city
-router.post('/city', cityController.create);
-router.patch('/city/:id', cityController.update);
-router.get('/city/:id', cityController.get);
-router.get('/city', cityController.getAll);
-router.delete('/city/:id', cityController.destroy);
+// // these are for city
+// router.post('/city', cityController.create);
+// router.patch('/city/:id', cityController.update);
+// router.get('/city/:id', cityController.get);
+// router.get('/city', cityController.getAll);
+// router.delete('/city/:id', cityController.destroy);
 
-// these are for flight
-router.post('/flights', FlightMiddleware.validateCreateFlight, flightController.create);
-router.patch('/flights/:id', flightController.update);
-router.get('/flights/:id', flightController.get);
-router.delete('/flights/:id', flightController.destroy);
+// // these are for flight
+// router.post('/flights', FlightMiddleware.validateCreateFlight, flightController.create);
+// router.patch('/flights/:id', flightController.update);
+// router.get('/flights/:id', flightController.get);
+// router.delete('/flights/:id', flightController.destroy);
 
-module.exports = router;
+// module.exports = router;
