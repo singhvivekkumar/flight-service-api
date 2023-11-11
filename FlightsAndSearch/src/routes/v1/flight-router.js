56f8c1c3699f1e1df1ed88361b8ee29a3ae1972f
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-11-11 18:26:50 +0530

    fixed flight api

--- FlightsAndSearch/src/routes/v1/flight-router.js
+++ FlightsAndSearch/src/routes/v1/flight-router.js
@@ -3,6 +3,11 @@ const flightController = require('../../controllers/flight-controller')
 
 const flightRouter = express.Router();
 
+// flightRouter.post('/flight', flightController.create);
+// flightRouter.patch('/flight/:id', flightController.update);
+// flightRouter.get('/flight/:id', flightController.get);
+// flightRouter.delete('/flight/:id', flightController.destroy);
+
 flightRouter.post('/flight', flightController.create);
 flightRouter.patch('/flight/:id', flightController.update);
 flightRouter.get('/flight/:id', flightController.get);
